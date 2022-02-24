4b86a963a81f34ecef43d19e6306a66d3854a19f
zohreh.sa79 <zohreh.s.m.a.n@gmail.com>
2022-02-24 16:02:35 +0330

    First commit

---
+++ website/Scripts/bookmarks/account/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from django.contrib.auth.views import *
+from . import views
+
+urlpatterns = [
+    # Post views
+    path('login/', views.user_login, name='login'),
+    path('login/',LoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+    path('',views.dashboard,name="dashboard"),
+    path('password_change/',PasswordChangeView.as_view(),name="password_change"),
+    path('password_change/done/',PasswordChangeDoneView.as_view(),name="password_change_done"),
+    path('password_reset/',PasswordResetView.as_view(),name="password_reset"),
+    path('password_reset/done/',PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name="password_reset_confirm"),
+    path('reset/done/',PasswordResetCompleteView.as_view(),name="password_reset_complete"),
+    path('register/',views.register,name="register"),
+    path('edit/',views.edit,name="edit"),
+    # path('',include('django.contrib.auth.urls')),
+]
